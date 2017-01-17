be1b78ad0126a3bd4d168544af9d75ceccb18753
qingxiaofei <qingxiaowei@outlook.com>
2017-01-17 16:20:31 +0800

    Update data.php

--- 3023api/data.php
+++ 3023api/data.php
@@ -12,7 +12,7 @@
         <p style="text-align: center;margin-bottom: 1.2rem;"><img src="<?php echo $json['img']?>"></p>
         <h1><?php  echo $json['model']?></h1>
         <ul>
-            <li>序列号：<span><?php echo $json['sn']?></span></li>
+            <li><?php if(isset($json['activated'])) echo '序列号';else echo 'IMEI';?>：<span><?php if(isset($json['activated'])) echo $json['sn'];else echo $sn;?></span></li>
             <li>设备型号：<span><?php echo $json['model']?></span></li>
             <li>容量：<span><?php echo $json['capacity']?></span></li>
             <li>颜色：<span><?php echo $json['color']?></span></li>
@@ -20,15 +20,29 @@
             <li>类型：<span><?php echo $json['identifier']?></span></li>
             <li>模型：<span><?php echo $json['order']?></span></li>
             <li>网络：<span><?php echo $json['network']?></span></li>
-            <li>激活状态：<span><?php if($json['activated']==0)echo '未激活';else echo '已激活';?></span></li>
-            <li>激活时间：<span><?php echo $json['time']?></span></li>
             <li>产地：<span><?php echo $json['origin']?></span></li>
-            <li>出厂日期：<span><?php echo $json['start'].'-'.$json['end']?></span></li>
-            <br/>
             <li>产品类型：<span><?php echo $json['product']?></span></li>
-            <li>有效购买日期：<span><?php if($json['purchasing']==0)echo '不是';else echo '是';?></span></li>
-            <li>硬件保修：<span><?php echo $json['warranty']?></span></li>
-            <li>保修剩余：<span><?php echo $json['daysleft']?></span></li>
+            <?php               
+                if(isset($json['activated'])){
+                    echo "<li>激活状态：<span>";
+                        if($json['activated']==0)echo '未激活';else echo '已激活';
+                    echo "</span></li>";
+					
+                    echo "<li>激活时间：<span>{$json['time']}</span></li>"; 
+					
+                    echo "<li>出厂日期：<span>{$json['start']}-{$json['end']}</span></li>";
+					
+                    echo '<br/>';  
+					
+                    echo "<li>有效购买日期：<span>";
+                        if($json['purchasing']==0)echo '不是';else echo '是';
+                    echo '</span></li>';
+					
+                    echo "<li>硬件保修：<span>{$json['warranty']}</span></li>";
+					
+                    echo "<li>保修剩余：<span>{$json['daysleft']}天</span></li>";
+                }           
+            ?>                       
 <!--            <li>电话支持：<span>--><?php //echo $json['tele']?><!--</span></li>-->
             <br/>
             <li class="li_title">规格参数：</li>
@@ -43,16 +57,21 @@
             <li>尺寸：<span><?php echo $json['spec']['dimension']?></span></li>
             <li>重量：<span><?php echo $json['spec']['weight']?></span></li>
             <br/>
-            <li class="li_title">翻新机鉴定：</li>
-            <li>翻新机概率： <span><?php echo $json['renovate']['probability']?></span></li>
-            <li>鉴定结果：<span><?php echo $json['renovate']['result']?></span></li>
-            <br>
-            <li><p style="font-size: 0.8rem;text-align: center;color: #ffffff">本次查询数据由 <a href="https://github.com/LittleFee/plugin/tree/master/3023api" style="color: #f2c919;text-decoration:none">LittleFee</a> 提供<br>结果仅供参考，以 <a href="https://checkcoverage.apple.com/?sn=<?php  echo $json['sn']?>" style="color: #f2c919;text-decoration:none">苹果官网查询</a> 为准</p></li>
+            <?php
+                if(isset($json['renovate'])){
+                    echo '<li class="li_title">翻新机鉴定：</li>';
+                    echo "<li>翻新机概率： <span>{$json['renovate']['probability']}</span></li>";
+                    echo "<li>鉴定结果：<span>{$json['renovate']['result']}</span></li>";
+                    echo '<br>';
+                }           
+            ?>           
+            <li>
+			    <p style="font-size: 0.8rem;text-align: center;color: #ffffff">
+				    本次查询数据由 <a href="https://github.com/LittleFee/plugin/" style="color: #f2c919;text-decoration:none">LittleFee</a> 提供<br>
+					结果仅供参考，以 <a href="https://checkcoverage.apple.com/?sn=<?php if(isset($json['activated'])) echo $json['sn'];else echo $sn;?>" style="color: #f2c919;text-decoration:none" target="_blank">苹果官网</a> 查询结果为准
+			    </p>
+			</li>
         </ul>
-
-
-
-    </div>
-
+    </div>s
 </body>
 </html>
