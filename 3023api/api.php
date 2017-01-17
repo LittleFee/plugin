f6e25c9d244d7598bfc4986aaf13b77a131dcf1a
qingxiaofei <qingxiaowei@outlook.com>
2017-01-17 16:21:23 +0800

    Update api.php

--- 3023api/api.php
+++ 3023api/api.php
@@ -4,21 +4,23 @@
         echo "<script>alert('请输入序列号');history.go(-1);</script>";
         exit;
     }
-    $filename = "./code.conf";//key文件地址
+    $filename = "./code.conf";//key文件地址，code.conf用于放置你的key 
     $handle = fopen($filename, "r");//打开并获得文件句柄
     $key = fread($handle, filesize ($filename));//获取key文件里的key值
     fclose($handle);
     $wrongCode=array(//错误状态
-        '302311'  => '序列号错误',
-        '302312'  => '非苹果序列号',
+        '302311'  => '序列号或者IEMI错误',
+        '302312'  => '非苹果序列号或者IEMI',
         '302301'  => 'Wrong key',//配置文件里的key缺失
         '302302'  => 'Wrong key',//配置文件里的key无效
         '302303'  => 'Insufficient account balance',//余额不足
         '302304'  => 'Wrong IP',//IP白名单限制
         '302305'  => 'Wrong sign'//签名错误
     );
-    $sn=$_GET['sn'];//获得序列号
-    $ch = curl_init("http://api.3023.com/apple/apple?sn={$sn}");
+    $sn=strtr($_GET['sn'], array(' '=>''));//获得输入的序列号或者IMEI，并去掉中间的空格，确保判断正确 
+    //$str=(is_numeric($sn))?'apple/imei?imei=':'apple/apple?sn=';//判断是否是纯数字      
+    $str=preg_match('/^[0-9]{15}$/',$sn)?'imei?imei=':'apple?sn=';//使用正则匹配  
+    $ch = curl_init("http://api.3023.com/apple/{$str}{$sn}");//组合api和请求数据的url
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
     curl_setopt($ch,CURLOPT_HTTPHEADER,array("3023-key:{$key}"));
     curl_setopt($ch,CURLOPT_ENCODING,"gzip");
@@ -31,10 +33,3 @@
         echo "<script>alert('{$wrongCode [$json['code']]}');history.go(-1);</script>";
         exit;
     }
-	
-/**
- * Created by PhpStorm.
- * User: qingxiaofee
- * Date: 2017/1/16
- * Time: 10:23
- */
